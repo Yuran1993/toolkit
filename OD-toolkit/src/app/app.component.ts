941975e88f1327f3443d814bc8b7f406ed7fafbd
Yuran van den Bosch <yuran.van.den.bosch@onlinedialogue.com>
2020-11-02 16:37:06 +0100

    added redirect to https

--- OD-toolkit/src/app/app.component.ts
+++ OD-toolkit/src/app/app.component.ts
@@ -1,11 +1,20 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'OD-toolkit';
+
+  ngOnInit() {
+    if (environment.production) {
+      if (location.protocol === 'http:') {
+        window.location.href = location.href.replace('http', 'https');
+      }
+    }
+  }
 }
